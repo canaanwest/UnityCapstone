653422c7b4ec96b757229d0310a1ff5ae603acec
canaanwest <canaan.west@gmail.com>
2018-01-15 20:24:58 -0800

    set up infrastructure for displaying helper words

--- capstone/Assets/_WordStuff/creation scene/DisplayPronouns.cs
+++ capstone/Assets/_WordStuff/creation scene/DisplayPronouns.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DisplayPronouns : MonoBehaviour {
+    public GameObject loadWordsHere;
+    public GameObject wordPrefab; 
     string[,] pronouns = {{"I", "me", "my", "mine", "I'm"},
         { "we", "we're", "us", "our", "ours"},
         { "they", "they're", "they", "their", "theirs"},
@@ -13,18 +16,44 @@ public class DisplayPronouns : MonoBehaviour {
     string[] articles = { "a", "an", "the", "this", "these", "those" };
 
 
-    string[,] verbs = {{ "was", "go", "want", "think", "want", "feel", "look", "wonder" },
-                        { "were", "go", "want", "think", "want", "feel", "look", "wonder"},
-                        { "was", "goes", "wants", "thinks", "wants", "feels", "looks", "wonders"}};
+    string[,] verbs = {{ "was", "go", "want", "think", "feel", "look", "wonder" },
+                        { "were", "go", "want", "think", "feel", "look", "wonder"},
+                        { "was", "goes", "wants", "thinks", "feels", "looks", "wonders"}};
 
 
 	// Use this for initialization
 	void Start () {
+        int randomOne = Random.Range(0, pronouns.Length);
+        int randomTwo = Random.Range(0, verbs.Length);
+
+        string[] pronoun = { pronouns[randomOne, 0], pronouns[randomOne, 1], pronouns[randomOne, 2],
+                            pronouns[randomOne, 3], pronouns[randomOne, 4] } ;
+        string[] verb = { verbs[randomTwo, 0], verbs[randomTwo, 1],
+            verbs[randomTwo, 2], verbs[randomTwo, 3], verbs[randomTwo, 4] };
+
+        LoadWords(pronoun, "Pronouns");
+        LoadWords(verb, "Verbs");
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+    void LoadWords(string[] words, string gameObject)
+    {
+        Transform gettingLoadObject = loadWordsHere.transform.Find(gameObject);
+
+        int i = 0;
+        foreach (Transform child in gettingLoadObject.transform)
+        {
+            Text toFill = wordPrefab.GetComponent<Text>();
+            toFill.text = words[i];
+
+            GameObject wordSpace = Instantiate(wordPrefab, child.transform.position, Quaternion.identity) as GameObject;
+            wordSpace.transform.parent = child;
+
+            print("Word prefab is " + wordPrefab);
+
+        }
+        
+    
+    }
 }
