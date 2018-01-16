364841d57ffc8c95cbe21826f7cbfbd06abb4356
canaanwest <canaan.west@gmail.com>
2018-01-15 18:24:10 -0800

    got raycasts working on 2d object

---
+++ capstone/Assets/MoveToNotebook.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveToNotebook : MonoBehaviour {
+    public BoxCollider2D collider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown(0))
+        {
+            SelectWordForMovement();
+        }	
+	}
+
+    void SelectWordForMovement()
+    {
+        Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        print("worldpoint is : " + worldPoint);
+        RaycastHit2D hit = Physics2D.Raycast(Input.mousePosition, new Vector2(0, 0));
+
+        
+        print("hit is " + hit);
+        if (hit.collider)
+        {
+            print("You hit a word!");
+            print(hit.collider.transform.GetComponent<Text>());
+        }
+    }
+
+
+}
---
+++ capstone/Assets/_Scripts/SackMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SackMovement : MonoBehaviour {
+    Vector3 cameraFollowOffset = new Vector3(5, 1, 5);
+    Camera followingCamera;
+
+    // Use this for initialization
+    void Start () {
+        followingCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(followingCamera.transform.position);
+        print(followingCamera.transform.eulerAngles);
+
+        this.transform.position = followingCamera.transform.position + cameraFollowOffset;
+	}
+}
